7da1a2db20d315223062d5d32d8ff25271684745
Faisal Pasha <mfaisalpasha@gmail.com>
2022-02-17 14:02:32 +0900

    added lambda python package structure

--- python.sh
+++ python.sh
@@ -4,3 +4,14 @@ python3 -m venv <name_of_virtualenv>
 source env/bin/activate
 # Deactivate venv
 deactivate
+
+# Lambda with third party packages
+pip install --target ./package requests
+# my-deployment-package.zip$
+#   │ lambda_function.py
+#   │ __pycache__
+#   │ certifi/
+#   │ certifi-2020.6.20.dist-info/
+#   │ chardet/
+#   │ chardet-3.0.4.dist-info/
+#   ...
